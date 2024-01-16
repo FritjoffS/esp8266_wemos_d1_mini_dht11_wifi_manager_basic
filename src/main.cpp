e666f26fe8c246a262a3eeec891299a562d8e878
FritjoffS <attholmen@gmail.com>
2024-01-16 21:18:50 +0100

    changed monitor speed

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void setup() {
     // it is a good practice to make sure your code sets wifi mode how you want it.
  
     // put your setup code here, to run once:
-    Serial.begin(9600);
+    Serial.begin(115200);
     
     //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wm;
